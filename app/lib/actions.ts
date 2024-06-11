61f0a27922bbd3003ecac20159ca00e035d5cc9c
Jun Zhang <jun@junz.org>
2024-06-11 10:39:45 +0800

    error handling
    Signed-off-by: Jun Zhang <jun@junz.org>

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,12 +22,17 @@ export async function createInvoices(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+        revalidatePath('/dashboard/invoices');
+        redirect('/dashboard/invoices');
+    } catch (error) {
+        return { messege: 'Fail to create invoice' };
+    }
+
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -40,18 +45,29 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
 
     const amountInCents = amount * 100;
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+
+        revalidatePath('/dashboard/invoices');
+        redirect('/dashboard/invoices');
+
+    } catch (error) {
+        return { message: 'Fail to update invoice' };
+    }
 
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+    } catch (error) {
+        return { message: 'Fail to delete invoice' };
+    }
+
 }
